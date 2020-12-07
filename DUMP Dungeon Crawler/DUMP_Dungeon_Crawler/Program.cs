7c77967c8611510b24a82dee518e1fc2707a0873
MislavLesin <pripravnik-mislav.lesin@dump.hr>
2020-12-07 17:59:42 +0100

    First commit, project setup

---
+++ DUMP Dungeon Crawler/DUMP_Dungeon_Crawler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DUMP_Dungeon_Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DUMP_Dungeon_Crawler.Presentation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DUMP_Dungeon_Crawler.Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("We meet again");
+        }
+    }
+}
